f9d29519b1d93d7f50107e157e55ac79e9389032
Swati Gupta <65848172+SwatiInd@users.noreply.github.com>
2020-06-25 09:14:46 +0100

    Update RCode.R

--- RCode.R
+++ RCode.R
@@ -9,7 +9,8 @@ data$date <- as.Date(data$date)
 # interval: identifier for 5-minute interval 
 
 ## Analysis of total steps per day
-step_day <- data %>% group_by(date) %>% summarize(stepsperday = sum(steps, na.rm = TRUE))
+data_good <- subset(data, !is.na(steps))
+step_day <- data_good %>% group_by(date) %>% summarize(stepsperday = sum(steps, na.rm = TRUE))
 with(step_day, hist(stepsperday, breaks = 25, xlim = c(0, 25000), ylim = c(0,12),
                     xlab = "Steps per day", ylab = "Number of days", 
                     main = "Histogram of steps per day"))
@@ -18,14 +19,16 @@ mean <- mean(step_day$stepsperday)
 median <- median(step_day$stepsperday)
 
 ## Analysis of daily activity pattern
-step_pattern <- data %>% group_by(interval) %>% summarize(stepsperinterval = mean(steps, na.rm = TRUE))
+step_pattern <- data_good %>% group_by(interval) %>% 
+                    summarize(stepsperinterval = mean(steps))
 
-step_pattern$time <- sprintf('%04d', step_pattern$interval) %>% strptime("%H%M")
-with(step_pattern, plot(time, stepsperinterval, type = 'l', xlab = "Time", 
+# step_pattern$time <- sprintf('%04d', step_pattern$interval) %>% strptime("%H%M")
+with(step_pattern, plot(interval, stepsperinterval, type = 'l', xlab = "Time", 
                         ylab = "Average no. of steps", main = "Average steps per Interval"))
 
 max_index <- which.max(step_pattern$stepsperinterval)
-max_interval <- step_pattern$time[max_index] %>% strftime(max_interval, format="%H:%M")
+
+# max_interval <- step_pattern$time[max_index] %>% strftime(max_interval, format="%H:%M")
 
 ## NA missing values
 na_sum <- sum(is.na(data))
@@ -38,9 +41,23 @@ data2$stnew[na_index] <- step_pattern$stepsperinterval[sapply(na_interval,
                         function(x){which(step_pattern$interval == x)})]
 
 step_day_new <- data2 %>% group_by(date) %>% summarize(stepsperday = sum(stnew))
-with(step_day_new, hist(stepsperday, breaks = 25, xlim = c(0, 25000), ylim = c(0,14),
+with(step_day_new, hist(stepsperday, breaks = 25, xlim = c(0, 25000), ylim = c(0,20),
                     xlab = "Steps per day", ylab = "Number of days", 
                     main = "Histogram of steps per day"))
 
 mean_new <- mean(step_day_new$stepsperday)
 median_new <- median(step_day_new$stepsperday)
+
+# factoring new data based on weekend and weekday
+weekday <- c("Monday",  "Tuesday" ,  "Wednesday", "Thursday"  , "Friday" )
+weekend <- c("Saturday", "Sunday")
+data2$type <- factor(sapply(data2$date, function(x) {if(weekdays(x) %in% weekday) "Weekday" else "Weekend"}))
+
+step_pattern_imputed <- data2 %>% group_by(interval, type) %>% 
+    summarize(stepsperinterval = mean(stnew))
+
+step_pattern_imputed$time <- as.POSIXct(sprintf('%04d', step_pattern_imputed$interval) %>% strptime("%H%M"))
+q <- with(step_pattern_imputed, qplot(interval, stepsperinterval, facets = type~., 
+                                      xlab = "Interval",ylab = "Average no. of steps", 
+                                      main = "Average steps per Interval", geom = "path"))
+plot(q)
